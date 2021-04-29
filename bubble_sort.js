b178af107b0a9b4df34b399902b9a236c702c58b
afifa-khatun <afifamasumem@gmail.com>
2021-04-29 08:43:05 -0700

    searching added

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,5 +1,5 @@
 
-
+// bubble sort implement
 const bubbleSort = (arr) => {
     for(let i = 0 ; i<arr.length; i++){
         for(let j = 0 ; j<arr.length; j++){
@@ -15,4 +15,23 @@ const bubbleSort = (arr) => {
 
 const sortedArray = bubbleSort([4,29,1,2,30]);
 console.log(sortedArray);
+
+
+
+// searching
+
+const searching = (value, arr) => {
+    for(let i = 0 ; i<arr.length ; i++){
+        if(arr[i] === value){
+            return 'Got the expected value.'
+            break ;
+        }else{
+            return 'Sorry not found!';
+            break ;
+        }
+    }
+}
+
+const result = searching('abc', ['ppc','dfdr','abc','ee']);
+console.log(result);
 
