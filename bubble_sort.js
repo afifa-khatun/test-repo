3e1ffc2dce1eb5e7662e9b18da83eebb97456f97
afifa-khatun <afifamasumem@gmail.com>
2021-04-29 08:33:07 -0700

    bubble sort

---
+++ bubble_sort.js
@@ -0,0 +1,18 @@
+
+
+const bubbleSort = (arr) => {
+    for(let i = 0 ; i<arr.length; i++){
+        for(let j = 0 ; j<arr.length; j++){
+            if(arr[j] > arr[j+1]){
+                const temp = arr[j];
+                arr[j] = arr[j+1]
+                arr[j+1] = temp ;
+            }
+        }
+    }
+    return arr ;
+}
+
+const sortedArray = bubbleSort([4,29,1,2,30]);
+console.log(sortedArray);
+
